e6ea820d4165b4b4d6329d9701d5062e4ee9ea55
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-17 22:19:02 -0500

    Concatenating Strings - using an extra + sign and placing string symbol " "

--- Week 2/Basic Expressions/js/script.js
+++ Week 2/Basic Expressions/js/script.js
@@ -39,10 +39,19 @@
 
 // Expressions - Expressions with Arrays
 
-var orangeBins = [234, 567, 883]
+//var orangeBins = [234, 567, 883]
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
+//var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+//console.log(total);
+
+
+//Expressions - Concatenating Strings
+
+var firstName = "Wess";
+var lastName = "Pedigo";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
 
 
 
