9ba1f781cf276d30f27ff49c82f000d2ca5eec1b
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 00:18:40 -0500

    Created individual folder for each problem within the Expressions_worksheet folder.

---
+++ Expressions_worksheet/Dog Years/js/script.js
@@ -0,0 +1,24 @@
+// Wess Pedigo
+// July 17,2013
+// Expression Worksheet
+
+// Dog Years
+//Calculate how old Sparky the pit bull is in human years.
+
+var humanAge = 3; //This is how old Sparky is in human years.
+var dogYears = 7; //Dogs age 7 years faster than humans.
+
+//Multiply Sparky's age of 3 times 7 dog years. 
+
+var sparkyAge = humanAge * dogYears;
+
+//Print out sparky's age in dog years.   
+console.log("Sparky is " + humanAge + " human years old which is " + sparkyAge + " in dog years.");
+
+
+
+
+
+
+
+
