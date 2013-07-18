31099548cc4763bc67fe2e550b10905c1e41e07a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 02:04:00 -0500

    Added a <BR> in order to show the next part of the question on the next line.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -9,7 +9,7 @@ var groceryBills = [235, 333, 274, 169, 131]; //created array and input values f
 
 var groceryTotal = groceryBills[0] + groceryBills[1] + groceryBills[2] + groceryBills[3] + groceryBills[4];  // adding all grocery bills with index numbers.  
 
-console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks.)
+console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks." "<BR>");
 
 
 //Print total amount spent on groceries over 5 weeks and also provide the average per //week.  
