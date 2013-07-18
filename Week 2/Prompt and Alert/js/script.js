3c17141ea54dc71accf81072b570fa822fc3f4e1
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 18:35:36 -0500

    Changed the index files title tag. Also, created a an alert and prompt.

--- Week 2/Prompt and Alert/js/script.js
+++ Week 2/Prompt and Alert/js/script.js
@@ -3,7 +3,19 @@
 // 
 // Screencast: Prompt and Alert
 
-prompt("Enter your year of birth:");
+// var userInput = prompt("Enter your year of birth:");
+
+// console.log(userInput);
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+
+var area = width * height;  //calculates with that info
+
+//console.log(area);   //prints it out to the console
+
+alert("The area of your rectangle is " + area);
+
 
 
 
