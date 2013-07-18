89bfcc9664cf3c2dc7c6de3a675a91e73fe9a54e
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 02:00:41 -0500

    added console.log to print total amount of groceries spent over five weeks.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -9,12 +9,12 @@ var groceryBills = [235, 333, 274, 169, 131]; //created array and input values f
 
 var groceryTotal = groceryBills[0] + groceryBills[1] + groceryBills[2] + groceryBills[3] + groceryBills[4];  // adding all grocery bills with index numbers.  
 
-console.log(groceryTotal)
+console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks.)
 
 
 //Print total amount spent on groceries over 5 weeks and also provide the average per //week.  
 
-console.log("You have spent a total of " +  + " on groceries over 5 weeks. That is an average of " + + "per week");
+//console.log("You have spent a total of $" +  + " on groceries over 5 weeks. That is an average of " + + "per week");
 
 
 
