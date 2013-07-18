7aa5d4e08996afba3dbf911d6777286b9b4e794a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 16:53:00 -0500

    Added console.lot with string and places for variables.

--- Expressions_worksheet/Discounts/js/script.js
+++ Expressions_worksheet/Discounts/js/script.js
@@ -2,24 +2,13 @@
 // July 17,2013
 // Expression Worksheet
 
-// Average Shopping Bill
-// Calculate the total amount spent on groceries over 5 weeks and calculate average.
+// Discounts
+// 
 
-var groceryBills = [235, 333, 274, 169, 131]; //created array and input values for each week.
-
-var groceryTotal = groceryBills[0] + groceryBills[1] + groceryBills[2] + groceryBills[3] + groceryBills[4];  // adding all grocery bills with index numbers.  
-
-console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks." + "<BR>");
-
-var weekAverage = groceryTotal / 5;  //total of groceries divided by 5 will give the average.
-
-//Print total amount spent on groceries over 5 weeks and also provide the average per //week.  
-
-console.log("That is an average of $" + weekAverage + " per week.");
+var 
 
 
-
-
+console.log("Your " + + "was originally $" + + " but after a " + + " discount, it is now $" + + " with tax."
 
 
 
