01e55822778f8d5a887b3fd57509593f3aadbef8
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 18:45:48 -0500

    Added another variable and added a prompt with a string asking the user to enter in interest rate on the loan.

--- WPF Class/Expressions_Wacky/js/script.js
+++ WPF Class/Expressions_Wacky/js/script.js
@@ -6,7 +6,12 @@
 // Calculating the monthly payment for a car loan.
 // 
 
-var carPrice = prompt("Please enter the price of the car", 24,000);
+var carPrice = prompt("Please enter the price of the car", 24,000);  //having user input value of car
+
+var interestRate = prompt("Please enter the interest rate on the loan", 0%);  //asking the user to input the interest rate for the loan.
+
+// var numberOfMonths = prompt("Please enter the number of monthly payments", 36);  
+
 
 
 
