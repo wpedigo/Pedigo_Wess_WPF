11909b6539de96c52ea187bb1128d074d000b265
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-17 20:09:42 -0500

    Changed alert to add numbers and strings.

--- Week 2/Temperature Converter  User Friendly/js/script.js
+++ Week 2/Temperature Converter  User Friendly/js/script.js
@@ -1,12 +1,12 @@
 // Fahrenheit to Centrigrade 
 // Equation is ºC = 5/9 (ºF - 32).
 
-var degFahren = prompt("Enter the degree in Fahrenheit", "");
+var degFahren = prompt("Enter the degree in Fahrenheit", 50);
 var degCent;
 
 degCent = 5/9 * (degFahren - 32);
 
-alert(degCent);
+alert(degFahren + "\xB0 Fahrenheit is " + degCent + "\xB0 centigrade");
 
 
 
