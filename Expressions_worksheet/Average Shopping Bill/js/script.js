b1a4003b3cb19cd006c2ddb15807c4df2d39ebc9
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-18 02:17:33 -0500

    Completed the console.log with + sign where it was missing on line 12.

--- Expressions_worksheet/Average Shopping Bill/js/script.js
+++ Expressions_worksheet/Average Shopping Bill/js/script.js
@@ -9,13 +9,13 @@ var groceryBills = [235, 333, 274, 169, 131]; //created array and input values f
 
 var groceryTotal = groceryBills[0] + groceryBills[1] + groceryBills[2] + groceryBills[3] + groceryBills[4];  // adding all grocery bills with index numbers.  
 
-console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks." "<BR>");
+console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks." + "<BR>");
 
 var weekAverage = groceryTotal / 5;  //total of groceries divided by 5 will give the average.
 
 //Print total amount spent on groceries over 5 weeks and also provide the average per //week.  
 
-console.log("That is an average of " + weekAverage + " per week.");
+console.log("That is an average of $" + weekAverage + " per week.");
 
 
 
