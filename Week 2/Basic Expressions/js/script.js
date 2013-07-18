eff0bb2227868581b8beaabb8dcf89dc812ba77c
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-17 21:48:28 -0500

    Arithmetic Operators - Area of a triangle, also added "area =" + area

--- Week 2/Basic Expressions/js/script.js
+++ Week 2/Basic Expressions/js/script.js
@@ -1,7 +1,25 @@
 //Basic Expressions
+//
+// var a = 2; //sets up our variable a and defines it with 2
+// a = a + 3; //add 3 to a
+// console.log(a); 
 
-var a = 2;
-console.log(a);
+//var yearBorn = 1979;
+//var age = 2013 - yearBorn - 1;
+//console.log(age);
+
+
+//Expressions - Arithmetic Operators
+// area of the triangle is half the width times the height
+
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log("Area = " + area);
+
+
+
+
 
 
 
