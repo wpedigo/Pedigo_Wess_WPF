78dae0f79c204c4fe9e2c6152eb68cbd16b7e018
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-17 22:05:10 -0500

    Average of 4 numbers - Also, example of PEMDAS (order of operations)

--- Week 2/Basic Expressions/js/script.js
+++ Week 2/Basic Expressions/js/script.js
@@ -17,8 +17,18 @@
 //var area = width * height/2;
 //console.log("Area = " + area);
 
-var remainder = 211 % 8;
-console.log(remainder);
+//var remainder = 211 % 8;
+//console.log(remainder);
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+// PEMDAS - The order of operations
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average);
 
 
 
