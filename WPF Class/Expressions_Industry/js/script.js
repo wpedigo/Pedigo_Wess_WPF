6d7f2c39939bc363f2c92ef7753fcf2f75c6d44a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 18:35:42 -0500

    Added comment : //Expressions_Industry at top of script with my name and date.

--- WPF Class/Expressions_Industry/js/script.js
+++ WPF Class/Expressions_Industry/js/script.js
@@ -2,7 +2,23 @@
 // July 18,2013
 // Assignment: Expressions
 
+// Calculate Profit based on Cost, Retail and Units sold for an ecommerce store
 
+var cost = prompt("Please enter the cost of the product", 25);
+var retail = prompt("Please enter the retail price", 50);
+var units = prompt("Please enter units sold", 2);
+
+// Set profit to retail
+var profit = retail;  
+
+// Subtract cost
+profit -= cost;
+
+//Multiply by units sold
+profit *= units
+
+// Make Profit
+console.log("You have a profit of $" + profit.toFixed(2));
 
 
 
