76f96ac38a83efa88ff0e57e854b676c799d1c63
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 21:11:25 -0500

    Added the closing parentheses to the prompt and will test it.

--- WPF Class/Expressions_Wacky/js/script.js
+++ WPF Class/Expressions_Wacky/js/script.js
@@ -8,6 +8,6 @@
 var batter1 = new array();   //Pre-setting array variable
 var batter2 = new array();   //Pre-setting array for second variable.
 
-batter1["atBats"] = prompt("How many at bats does the first batter have?",500;  //prompts the user to enter in how many at bats batter has.
+batter1["atBats"] = prompt("How many at bats does the first batter have?",500);  //prompts the user to enter in how many at bats batter has.
 
-batter1["hits"] = prompt("How many hits does batter 1 obtain.","");
+batter1["hits"] = prompt("How many hits does batter 1 get?","");
