af2b9936b2bdf786a64743194ca94a942d810043
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 20:40:38 -0500

    Changed the holding place within the prompt with a value of 36, in order to give the user an example of what to enter.

--- WPF Class/Expressions_Personal/js/script.js
+++ WPF Class/Expressions_Personal/js/script.js
@@ -9,7 +9,7 @@ var carPrice = prompt("Please enter the price of the car.", 24000);  //asking th
 
 var interestRate = prompt("Please enter the interest rate on the loan.",5);   //asking the user to input the interest rate on the loan. 
 
-var numberOfMonths = prompt("Please enter the number of monthly payments.","");   //asking the user to input the number of monthly payments that will be used to payoff the loan.
+var numberOfMonths = prompt("Please enter the number of monthly payments.",36);   //asking the user to input the number of monthly payments that will be used to payoff the loan.
 
 //The equation used to calculate the monthly payments is principle(rate / 12) divided by (1 - (1 + rate / 12)) to the power of -m.
 
