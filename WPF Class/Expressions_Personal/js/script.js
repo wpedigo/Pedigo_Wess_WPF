e1760a89cd8779c6f72326059f92a9e532875528
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 20:27:36 -0500

    Added third variable for number of monthly payments.

--- WPF Class/Expressions_Personal/js/script.js
+++ WPF Class/Expressions_Personal/js/script.js
@@ -9,7 +9,7 @@ var carPrice = prompt("Please enter the price of the car.", 24000);  //asking th
 
 var interestRate = prompt("Please enter the interest rate on the loan.",5);   //asking the user to input the interest rate on the loan. 
 
-
+var numberOfMonths = "Number of monthly payment";
 
 
 
