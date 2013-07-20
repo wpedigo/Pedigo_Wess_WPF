7b887f48f2aa6f1e7cca0e1e7ee8866f2b3eefca
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 19:07:28 -0500

    replaced the " " after the comma with the number 25, so it shows an example of what to place in the prompt box.

--- WPF Class/Expressions_Industry/js/script.js
+++ WPF Class/Expressions_Industry/js/script.js
@@ -5,7 +5,7 @@
 
 //Calculating profit base on cost, retail, and units sold.  Equation would be common when working as a web designer at an E-commerce store online, which is within my industry.
 
-var cost = prompt("Please enter the cost of the product", "");
+var cost = prompt("Please enter the cost of the product", 25);   
 
 
 
