34a0a49acaa5c451ada06e26995e9ccd3529c2f8
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 19:25:10 -0500

    Added periods to my prompts to end the sentences.

--- WPF Class/Expressions_Industry/js/script.js
+++ WPF Class/Expressions_Industry/js/script.js
@@ -5,9 +5,9 @@
 
 //Calculating profit base on cost, retail, and units sold.  Equation would be common when working as a web designer at an E-commerce store online, which is within my industry.
 
-var cost = prompt("Please enter the cost of the product", 25);   //created the first variable and a prompt box in order for the user to input how much the cost of the product is to the store selling the product. 
+var cost = prompt("Please enter the cost of the product.", 25);   //created the first variable and a prompt box in order for the user to input how much the cost of the product is to the store selling the product. 
 
-var retail = prompt("Please enter the retail price of the product",50);  //created the second variable and a prompt box in order for the user to input how much the product will sell for at retail. 
+var retail = prompt("Please enter the retail price of the product.",50);  //created the second variable and a prompt box in order for the user to input how much the product will sell for at retail. 
 
 var units = prompt("Please enter the number of units sold.", 2);
 
