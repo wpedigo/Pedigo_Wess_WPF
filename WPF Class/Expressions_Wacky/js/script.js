f5fec43f3cb90ea86f0f60c9de6dc01c22a05ef2
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 21:36:00 -0500

    Created the same equation for batter 2.

--- WPF Class/Expressions_Wacky/js/script.js
+++ WPF Class/Expressions_Wacky/js/script.js
@@ -3,7 +3,7 @@
 // Assignment: Expressions
 // Expressions_Wacky
 
-// Calculating the average of a baseball batter.
+// Calculating a baseball player's hitting average.
 
 var batter1 = new Array();   //Pre-setting array variable
 var batter2 = new Array();   //Pre-setting array for second variable.
@@ -17,4 +17,5 @@ batter2["atBats"] = prompt("How many at bats did the second batter have?","");  
 batter 2["hits"] = prompt("How many hits does batter 2 generate?",""); //added the last prompt for the user to enter in how many hits the batter generated. 
 
 var batter1Avg = batter1["hits"] / batter1["atBats"];   //created a variable with the value of an equation, which provides the batter's hitting average.
-var batter2Avg = "This is batter 2's hitting average.";
+
+var batter2Avg = batter2["hits"] / batter2["atBats"];  //created the same equation from batter one to batter 2.
