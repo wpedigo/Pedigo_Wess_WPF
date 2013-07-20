dec04aa7729a7f98aa0f105308164295d7f409e9
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 21:07:58 -0500

    Gave a value, which is a string, to the next item in the array.

--- WPF Class/Expressions_Wacky/js/script.js
+++ WPF Class/Expressions_Wacky/js/script.js
@@ -9,4 +9,5 @@ var batter1 = new array();   //Pre-setting array variable
 var batter2 = new array();   //Pre-setting array for second variable.
 
 batter1["atBats"] = prompt("How many at bats does the first batter have?",500;  //prompts the user to enter in how many at bats batter has.
-batter1[1] = "hits";
+
+batter1["hits"] = "Number of hits by batter 1";
