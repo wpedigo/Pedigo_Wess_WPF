6d9dde78bdc21287c72300b87bec8a0bc25a3dfb
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 20:52:20 -0500

    Added a comment stating what the calculation is about.

--- WPF Class/Expressions_Wacky/js/script.js
+++ WPF Class/Expressions_Wacky/js/script.js
@@ -3,28 +3,7 @@
 // Assignment: Expressions
 // Expressions_Wacky
 
-// Calculating the monthly payment for a car loan.
-// 
-
-var carPrice = prompt("Please enter the price of the car", 24,000);  //having user input value of car
-
-var interestRate = prompt("Please enter the interest rate on the loan", 0%);  //asking the user to input the interest rate for the loan.
-
-var numberOfMonths = prompt("Please enter the number of monthly payments", 36);  //asking the user to input the number of monthly payments.
-
-// equation for monthly is  principle(rate/12) divided by (1 - (1 + rate / 12) power of -m )
- 
-
-
-
-
-
-
-
-
-
-
-
+// Calculating the average of a baseball batter.
 
 
 
