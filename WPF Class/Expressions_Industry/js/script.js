bd9c039df4bbf8645b48bc3b50cc7217ff766f48
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 19:00:17 -0500

    Added a comment about what I'm calculating.

--- WPF Class/Expressions_Industry/js/script.js
+++ WPF Class/Expressions_Industry/js/script.js
@@ -1,24 +1,13 @@
 // Wess Pedigo
 // July 18,2013
 // Assignment: Expressions
+// Expressions_Industry
 
-// Calculate Profit based on Cost, Retail and Units sold for an ecommerce store
+//Calculating profit base on cost, retail, and units sold.  Equation would be common when working as a web designer at an E-commerce store online, which is within my industry.
 
-var cost = prompt("Please enter the cost of the product", 25);
-var retail = prompt("Please enter the retail price", 50);
-var units = prompt("Please enter units sold", 2);
+var cost = prompt("Please enter the cost of the product.",25);
 
-// Set profit to retail
-var profit = retail;  
 
-// Subtract cost
-profit -= cost;
-
-//Multiply by units sold
-profit *= units
-
-// Make Profit
-console.log("You have a profit of $" + profit.toFixed(2));
 
 
 
