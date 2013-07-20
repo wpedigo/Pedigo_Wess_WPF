a45a70b20f087a2b4ffbc9889f57717b13404726
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 20:04:12 -0500

    Added first variable carPrice.

--- WPF Class/Expressions_Personal/js/script.js
+++ WPF Class/Expressions_Personal/js/script.js
@@ -1,6 +1,13 @@
 // Wess Pedigo
 // July 18,2013
 // Assignment: Expressions
+// Expressions_Personal
+
+// Calculating the monthly payment for a car loan.
+
+var carPrice = "Price of Car";
+
+
 
 
 
