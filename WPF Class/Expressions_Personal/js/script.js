fa8a34d07552a45a5b43f96f2b2c79e5c14af719
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-19 20:30:12 -0500

    Commented the file due to remember why variable was created and that the prompt is being used for the user to enter information to be calculated.

--- WPF Class/Expressions_Personal/js/script.js
+++ WPF Class/Expressions_Personal/js/script.js
@@ -9,7 +9,7 @@ var carPrice = prompt("Please enter the price of the car.", 24000);  //asking th
 
 var interestRate = prompt("Please enter the interest rate on the loan.",5);   //asking the user to input the interest rate on the loan. 
 
-var numberOfMonths = prompt("Please enter the number of monthly payments.","");
+var numberOfMonths = prompt("Please enter the number of monthly payments.","");   //asking the user to input the number of monthly payments that will be used to payoff the loan.
 
 
 
