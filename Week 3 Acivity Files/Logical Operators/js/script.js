22df887db6bfd4eade72be41afd56c00236d0e6e
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 15:19:09 -0500

    Created if/else statement to buy the smartphone.

---
+++ Week 3 Acivity Files/Logical Operators/js/script.js
@@ -0,0 +1,19 @@
+// Wess Pedigo
+// July 21,2013
+
+
+
+// Conditional Logic - Logical Operators
+
+var budget = 300;
+var iSmartPhonePrice = 199.99;
+var payCheck = 200;
+
+//if the price of the phone is less than our budget
+
+if(iSmartPhonePrice < budget){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you");
+}
+
