d99b74101323783f29f99b4ed837f70710418aab
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 11:45:01 -0500

    Moved Assignments to this folder, in order to keep them seperat from my exercise files.

---
+++ WPF Class - Assignments/Expressions_Personal/js/script.js
@@ -0,0 +1,30 @@
+// Wess Pedigo
+// July 18,2013
+// Assignment: Expressions
+// Expressions_Personal
+
+// Calculating the monthly payment for a car loan.
+
+var carPrice = prompt("Please enter the price of the car.", 24000);  //asking the user to input the value of the vehicle.
+
+var interestRate = prompt("Please enter the interest rate on the loan.",5);   //asking the user to input the interest rate on the loan. 
+
+var numberOfMonths = prompt("Please enter the number of monthly payments.",36);   //asking the user to input the number of monthly payments that will be used to payoff the loan.
+
+//The equation used to calculate the monthly payments is principle(rate / 12) divided by (1 - (1 + rate / 12)) to the power of m.
+
+var monthlyPayment = (carPrice * (interestRate / 12)) / (1 - (1 + (interestRate / 12))^numberOfMonths);
+
+console.log("Your monthly payment for your loan is $" + monthlyPayment.toFixed(2) + ". Enjoy your new car!");     // This will print out the resolution to the console after all the inputs are calculated.
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WPF Class - Assignments/Expressions_worksheet/Discounts/js/script.js
@@ -0,0 +1,26 @@
+// Wess Pedigo
+// July 17,2013
+// Expression Worksheet
+
+// Discounts
+// Calculate 
+
+var originalPrice = x  // Original price of the item
+var discount = 
+var itemDescription = "socks";    //Description of item
+var salesTax = 
+
+var resultWithTax = 
+
+var resultWithoutTax =
+
+console.log("Your " + itemDescription + "was originally $" + orignalPrice + " but after a " + + " discount, it is now $" + + " with tax."
+
+
+
+
+
+
+
+
+
