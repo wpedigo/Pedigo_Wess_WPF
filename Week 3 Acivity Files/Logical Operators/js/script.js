4416e7668f964120c27e17dc056fec7e69c03c57
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 15:22:41 -0500

    Printed out two statements that really don't work.

--- Week 3 Acivity Files/Logical Operators/js/script.js
+++ Week 3 Acivity Files/Logical Operators/js/script.js
@@ -9,11 +9,15 @@ var budget = 300;
 var iSmartPhonePrice = 199.99;
 var payCheck = 200;
 
-//if the price of the phone is less than our budget
+//if the price of the phone is less than our budget and our paycheck is over 300
 
 if(iSmartPhonePrice < budget){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you");
 }
-
+if(payCheck > 300){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you!");
+}
