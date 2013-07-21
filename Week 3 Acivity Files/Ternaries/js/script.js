48767edbc324ec0554148d70d625ae69caa6f715
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 15:51:28 -0500

    Added Ternarie operator

---
+++ Week 3 Acivity Files/Ternaries/js/script.js
@@ -0,0 +1,19 @@
+// Wess Pedigo
+// July 21,2013
+
+
+
+// Ternaries
+
+var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+
+/*if(gpa > 2.0){
+	console.log("You can graduate!");
+}else{
+	console.log("GPA is too low!");
+}*/
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+
