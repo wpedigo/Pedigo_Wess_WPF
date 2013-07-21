d40309c91adaba5c5df7341e0e6abc423ac14ef9
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 15:57:52 -0500

    used ternaries instead of traditional conditional statement.

--- Week 3 Acivity Files/Ternaries/js/script.js
+++ Week 3 Acivity Files/Ternaries/js/script.js
@@ -5,15 +5,12 @@
 
 // Ternaries
 
-var gpa = 48;
+var age = 11;
 
-//if the gpa is over the min 2.0 score, the student can graduate
+var book;
 
-/*if(gpa > 2.0){
-	console.log("You can graduate!");
-}else{
-	console.log("GPA is too low!");
-}*/
+//if the child is under 10, they get green eggs and ham, otherwise they get the time machine.
 
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
 
+console.log(book);
