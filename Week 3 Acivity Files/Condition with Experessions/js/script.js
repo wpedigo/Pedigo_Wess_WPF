eeb29eb3357b18e4726aa6372fcc9dbccd949c80
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 12:22:00 -0500

    Condition with expressions.

---
+++ Week 3 Acivity Files/Condition with Experessions/js/script.js
@@ -0,0 +1,18 @@
+// Wess Pedigo
+// July 21,2013
+
+
+
+// condition with expressions
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over the height of 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+
+}
