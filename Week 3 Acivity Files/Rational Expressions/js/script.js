26159417d7cd9e6ef8aea0174bc5b65957812136
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 12:07:27 -0500

    Rational expreassions

---
+++ Week 3 Acivity Files/Rational Expressions/js/script.js
@@ -0,0 +1,25 @@
+// Wess Pedigo
+// July 21,2013
+
+//
+
+//var kidHeight = 50;
+
+//if the kid is over 48 inches in height
+
+//if(kidHeight > 48){
+	//code performed if condition is true
+//	console.log("You can ride the coaster!");
+//}
+
+
+// Rational Experessions
+
+var kidHeight = 40;
+
+//if the kid is over the height of 48 inches in height
+if(kidHeight > 48){
+	//code performed if condition is true
+	console.log("You can ride the coaster");
+
+}
