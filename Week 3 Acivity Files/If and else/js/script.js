cb2278f9555cfd9d7413122b67ca15900a2ae8bf
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 12:28:51 -0500

    Using if and else statements

---
+++ Week 3 Acivity Files/If and else/js/script.js
@@ -0,0 +1,21 @@
+// Wess Pedigo
+// July 21,2013
+
+
+
+// If and else
+
+var kidHeight = 48;
+var minHeight = 48;
+
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over the height of 48 inches in height
+if(kidHeight > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+
+}else{
+	//code performed if condition is false
+	console.log("Sorry kid, you've got some growing to do first!");
+}
