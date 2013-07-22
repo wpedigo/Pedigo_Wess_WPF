fe586c94116b06f8dcb25fa6074997cb1bb3570c
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 19:25:40 -0500

    Comment stating the question.  Then created a variable for mpg and commented.

--- WPF Class - Assignments/Conditionals_worksheet/Group 1 - Last Change for Gas!/js/script.js
+++ WPF Class - Assignments/Conditionals_worksheet/Group 1 - Last Change for Gas!/js/script.js
@@ -3,7 +3,11 @@
 // Conditionals_worksheet
 // Group 1 - Last Chance for Gas!
 
-//Can a driver make it across the desert with their current fuel?
+//Can a driver make it 200 miles across the desert with their current fuel?
+
+// Ask the user to input mpg 
+var mpg = Number(prompt("How many miles per gallon does your car get?",35);  
+
 
 
 
