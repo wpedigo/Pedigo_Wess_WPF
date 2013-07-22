9eefc1d19da1fae8ebe22bf95a47742c1eb75e86
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-21 21:04:44 -0500

    Added a third variable and comment asking the user for how many gallons of gass their tank will hold.

--- WPF Class - Assignments/Conditionals_worksheet/Group 1 - Last Change for Gas!/js/script.js
+++ WPF Class - Assignments/Conditionals_worksheet/Group 1 - Last Change for Gas!/js/script.js
@@ -8,6 +8,12 @@
 // Ask the user to input mpg 
 var mpg = Number(prompt("How many miles per gallon does your car get?",35);  
 
+// Ask the user to input percentage of gas left in the tank
+var gasLeftInTank = Number(prompt("What is the percentage of gass left in the tank",50);
+
+// Ask the user to input number of gallons of gas the tank will hold.
+var tankCapacity = Number(prompt("How many gallons of gas will your car hold?",15);
+
 
 
 
