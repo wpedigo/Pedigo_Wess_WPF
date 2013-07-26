2280c9e562665a1c00daf98fc84cca74d453ec17
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 18:30:47 -0500

    Added a comment stating what we are calculating and budget variable.

--- WPF Class - Assignments/Conditional_Industry/js/script.js
+++ WPF Class - Assignments/Conditional_Industry/js/script.js
@@ -5,3 +5,7 @@
 
 // Calculating the cost of a web design project and verifying if it's within a company's budget.
 
+// Client budget estimate calculator.
+
+var budget = Number(prompt("Please enter in the total budget for your project. Example $15,000", ""));
+
