dcd1b75c6dd57bd6682f4257dce8a3aae9fbb05a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 18:57:30 -0500

    added a variable to take the information from the validateBudget and display a message in console.

--- WPF Class - Assignments/Conditional_Industry/js/script.js
+++ WPF Class - Assignments/Conditional_Industry/js/script.js
@@ -13,9 +13,13 @@ var budget = Number(prompt("Please enter in the total budget for your project. E
 
 var validateBudget = budget != "" && !isNaN(budget) ? true : false;    //created ternary to validate if value for budget is blank or not a number(googled the isNaN function to determine if it's a number or not.) 
 
+var budgetMessage = validateBudget ? "Thank you. The number of managers you entered was: " + budget : "Sorry, but you have entered an invalid number for kills.";
+
 // how many project managers are there.
 
 var projectManager = Number(prompt("Please enter the number of project managers.", ""));
 //created a variable for project manager and a prompt.
+
+//validate the number of project managers.
 
 
