4adc767452c259e3dd51d45b843e92d53dc096eb
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 18:43:46 -0500

    added a comment - how many project managers are there.

--- WPF Class - Assignments/Conditional_Industry/js/script.js
+++ WPF Class - Assignments/Conditional_Industry/js/script.js
@@ -10,4 +10,9 @@
 var budget = Number(prompt("Please enter in the total budget for your project. Example $15,000", ""));
 
 //validate the number typed into budget
-var validateBudget = budget != "" && !isNaN(budget) ? true : false;
+
+var validateBudget = budget != "" && !isNaN(budget) ? true : false;    //created ternary to validate if value for budget is blank or not a number(googled the isNaN function to determine if it's a number or not.) 
+
+// how many project managers are there.
+
+
