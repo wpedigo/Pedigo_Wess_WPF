855c545892c447da822f3ebe48b7035e6be516ae
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 20:37:33 -0500

    asked the user to enter the 2nd round stroke count.

--- WPF Class - Assignments/Conditionals_Personal/js/script.js
+++ WPF Class - Assignments/Conditionals_Personal/js/script.js
@@ -5,9 +5,11 @@
 
 //Calculating my home course handicap
 
-//Ask the user to input their last five rounds of golf.
+//Ask the user to input the number of strokes for the first round of golf.
 	var firstRound = Number(prompt("Please enter the stroke count for the first round", ""));
 
 //validate the number of strokes
 	var validateFirstRound = firstRound != "" && !isNaN(firstRound) ? true : false;
 	
+//ask the user to input the number of stokes for the 2nd round of gold.
+	var secondRound = Number(prompt("Please enter the stroke count for the second round", ""));
