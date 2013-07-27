ad6e7b9577d36c30fdaf8a8a95ed2cd5b5ea0680
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-27 15:04:49 -0500

    created a do while loop.

--- Week 4 Activity Files/js/script.js
+++ Week 4 Activity Files/js/script.js
@@ -8,3 +8,14 @@ while (b > 0){    //checks the condition
 	console.log(b + " kegs on the wall, we can still party!");
 	b--;		//increments or decrements the index
 }
+
+
+console.log("=========== Do While Loops ===========");
+
+var c = 10;
+
+do{
+	console.log(c + " kegs on the wall");
+	c--;
+}
+while(c > 0);
