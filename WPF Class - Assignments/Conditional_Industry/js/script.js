c1a5e3fdc7fc79d046066a2f5c06b0524cc0ff7a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 19:03:04 -0500

    added a validate project manager variable and a ternary to see if the value of project manager is a number or blank.

--- WPF Class - Assignments/Conditional_Industry/js/script.js
+++ WPF Class - Assignments/Conditional_Industry/js/script.js
@@ -7,19 +7,23 @@
 
 // Client budget estimate calculator.
 
-var budget = Number(prompt("Please enter in the total budget for your project. Example $15,000", ""));
+	var budget = Number(prompt("Please enter in the total budget for your project. Example 	$15,000", ""));
 
 //validate the number typed into budget
 
-var validateBudget = budget != "" && !isNaN(budget) ? true : false;    //created ternary to validate if value for budget is blank or not a number(googled the isNaN function to determine if it's a number or not.) 
+	var validateBudget = budget != "" && !isNaN(budget) ? true : false;    //created 	ternary to validate if value for budget is blank or not a number(googled the isNaN  function to determine if it's a number or not.) 
 
-var budgetMessage = validateBudget ? "Thank you. The number of managers you entered was: " + budget : "Sorry, but you have entered an invalid number for kills.";
+	var budgetMessage = validateBudget ? "Thank you. The number of managers you entered 	was: " + budget : "Sorry, but you have entered an invalid number for kills.";
+	console.log(budgetMessage);
 
 // how many project managers are there.
 
-var projectManager = Number(prompt("Please enter the number of project managers.", ""));
+	var projectManager = Number(prompt("Please enter the number of project managers.", 	""));
+
 //created a variable for project manager and a prompt.
+	
 
 //validate the number of project managers.
-
+	var validateProjectManager = projectManager !+ "" && !isNaN(projectManager) ? true : 	false;
+	
 
