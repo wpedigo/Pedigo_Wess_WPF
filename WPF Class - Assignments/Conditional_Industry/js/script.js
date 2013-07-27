51318ca8d16aeb7d1b786499aff3d7d1251a84a8
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 19:13:47 -0500

    added variable to prompt the user to enter number designers.

--- WPF Class - Assignments/Conditional_Industry/js/script.js
+++ WPF Class - Assignments/Conditional_Industry/js/script.js
@@ -6,18 +6,17 @@
 // Calculating the cost of a web design project and verifying if it's within a company's budget.
 
 // Client budget estimate calculator.
-
 	var budget = Number(prompt("Please enter in the total budget for your project. Example 	$15,000", ""));
 
 //validate the number typed into budget
 
 	var validateBudget = budget != "" && !isNaN(budget) ? true : false;    //created 	ternary to validate if value for budget is blank or not a number(googled the isNaN  function to determine if it's a number or not.) 
-
+	
+//created a variable to print to the console wether the outcome was true or false.	
 	var budgetMessage = validateBudget ? "Thank you. The number you entered for your budget 	was: " + budget : "Sorry, but you have entered an invalid number for your budget.";
 	console.log(budgetMessage);
 
 // how many project managers are there.
-
 	var projectManager = Number(prompt("Please enter the number of project managers.", 	""));
 
 //created a variable for project manager and a prompt.
@@ -28,5 +27,10 @@
 	
 	var projectManagerMessage = validateProjectManager ? "Thank you.  The number of project 	managers you entered was: " + projectManager : "Sorry, but you have entered an invalid 	number for your budget.";
 	console.log(projectManagerMessage); 
+	
+//how many designers 
+	var designer = Number(prompt("Please enter the number of designers." ,""));
+	
+	
 	
 
