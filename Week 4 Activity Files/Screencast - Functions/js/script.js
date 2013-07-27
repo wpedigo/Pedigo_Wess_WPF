5540c5988347fa7bcd9eddde489754548f5e9216
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-27 18:52:37 -0500

    variable outside the function is scoped to the larger part of the document. the variable within the function is scoped to the calcArea function.

--- Week 4 Activity Files/Screencast - Functions/js/script.js
+++ Week 4 Activity Files/Screencast - Functions/js/script.js
@@ -43,14 +43,15 @@ calcArea();
 
 // Variable scope 
 
-	
-}
+
+var width = 5;
 
 function calcArea(){
 	var width = 20;
 	var height = 30;
 	var area = width * height;
-	console.log(area);
+	//console.log(area);
 }
-
+console.log(width);
+calcArea();
 
