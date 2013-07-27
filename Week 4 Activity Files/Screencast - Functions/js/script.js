a1945244ef6a7bf356ea1269b322213d044ef6db
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-27 18:37:56 -0500

    created a function that calculates area and prints to console.

--- Week 4 Activity Files/Screencast - Functions/js/script.js
+++ Week 4 Activity Files/Screencast - Functions/js/script.js
@@ -1,6 +1,18 @@
 // Screencast: Functions
 
-function functionName(){
+/* function functionName(){
 	//code the function runs
 }
+*/
 
+function outptMsg(){
+	console.log("Hello World");
+	
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
