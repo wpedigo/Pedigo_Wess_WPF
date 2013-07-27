360faf3f4963ba17bf385901067bd5c18e23c21b
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 22:10:03 -0500

    created another variable assign a string to true or false depending on the validateNumKills variable.

--- WPF Class - Assignments/Conditional_Wacky/js/script.js
+++ WPF Class - Assignments/Conditional_Wacky/js/script.js
@@ -10,3 +10,5 @@
 
 // validate the value the user inputed for the number of kills.
 	var validateNumKills = numKills != "" && !isNaN(numKills) ? true : false;
+
+	var killMessage = validateNumKills ? "Thank you.  The number of kills you entered was: " + numKills : "Sorry, but you have entered an invalid number for kills.";
