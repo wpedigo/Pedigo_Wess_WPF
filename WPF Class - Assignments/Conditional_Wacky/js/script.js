f498574d90fbb9e49e3c7450c562ef82a48fedf6
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 21:59:36 -0500

    created a variable prompting the user to enter in how many kills the plater received during the game.

--- WPF Class - Assignments/Conditional_Wacky/js/script.js
+++ WPF Class - Assignments/Conditional_Wacky/js/script.js
@@ -6,3 +6,4 @@
 // Calculating the kill death ratio for battlefield three on Xbox 360.
 
 // Ask the user to enter in the number of kills during a game.
+	var numKills = Number(prompt("Please input the total number of kills you got.", ""));
