f824efa8d6421962e71acabd483184c64ce4ce0a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-26 19:19:38 -0500

    created a variable to print the result to the console, if the user entered the correct or incorrect value.

--- WPF Class - Assignments/Conditional_Industry/js/script.js
+++ WPF Class - Assignments/Conditional_Industry/js/script.js
@@ -6,14 +6,14 @@
 // Calculating the cost of a web design project and verifying if it's within a company's budget.
 
 // Client budget estimate calculator.
-	var budget = Number(prompt("Please enter in the total budget for your project. Example 	$15,000", ""));
+	var budget = Number(prompt("Please enter in the total budget for your project. Example $15,000", ""));
 
 //validate the number typed into budget
 
 	var validateBudget = budget != "" && !isNaN(budget) ? true : false;    //created 	ternary to validate if value for budget is blank or not a number(googled the isNaN  function to determine if it's a number or not.) 
 	
 //created a variable to print to the console wether the outcome was true or false.	
-	var budgetMessage = validateBudget ? "Thank you. The number you entered for your budget 	was: " + budget : "Sorry, but you have entered an invalid number for your budget.";
+	var budgetMessage = validateBudget ? "Thank you. The number you entered for your budget was: " + budget : "Sorry, but you have entered an invalid number for your budget.";
 	console.log(budgetMessage);
 
 // how many project managers are there.
@@ -25,7 +25,7 @@
 //validate the number of project managers.
 	var validateProjectManager = projectManager != "" && !isNaN(projectManager) ? true : 	false;
 	
-	var projectManagerMessage = validateProjectManager ? "Thank you.  The number of project 	managers you entered was: " + projectManager : "Sorry, but you have entered an invalid 	number for your budget.";
+	var projectManagerMessage = validateProjectManager ? "Thank you.  The number of project managers you entered was: " + projectManager : "Sorry, but you have entered an invalid 	number for your budget.";
 	console.log(projectManagerMessage); 
 	
 //how many designers 
@@ -33,6 +33,9 @@
 	
 //validate the number entered for designers
 	var validateDesigner = designer != "" && !isNaN(designer) ? true : false;
+	
+	var designerMessage = validateDesigner ? "Thank you.  The number of designers entered was: " + designer : "Sorry, but you have entered an invalid number for designers.";
+	console.log(designerMessage);
 	
 	
 
