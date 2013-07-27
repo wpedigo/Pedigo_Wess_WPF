d2ac18e4d0d1fcc12f5bf13cf777005d2fca0aec
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-27 14:58:33 -0500

    created new activites folder

---
+++ Week 4 Activity Files/js/script.js
@@ -0,0 +1,10 @@
+// screencast: Loops (30m)
+
+console.log("======= Loops =====");
+
+var b = 10;    //sets the index
+
+while (b > 0){    //checks the condition
+	console.log(b + " kegs on the wall, we can still party!");
+	b--;		//increments or decrements the index
+}
