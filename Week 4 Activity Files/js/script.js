08b0e31150b4716c59d73b33948865686fb8d136
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-29 21:34:19 -0500

    commenting out

--- Week 4 Activity Files/js/script.js
+++ Week 4 Activity Files/js/script.js
@@ -1,6 +1,6 @@
 // screencast: Loops (30m)
 
-console.log("======= Loops =====");
+/*console.log("======= Loops =====");
 
 var b = 10;    //sets the index
 
@@ -17,5 +17,59 @@ var c = 10;
 do{
 	console.log(c + " kegs on the wall");
 	c--;
+}while(c > 0);
+
+
+console.log("======== For Loops ============");
+
+for (var i = 10 ; i > 0 ; i--){
+	console.log(i + " kegs on the wall");
+}*/
+
+/* var a = 1;
+
+do{
+	console.log(a); 
+	a++;
+}while(a < 10);
+*/
+
+// writing my first function
+
+/*function writeUserWelcome (userName){
+	console.log("Welcome to my website " + userName + "<br />");
+	console.log("Hope you enjoy it!");
 }
-while(c > 0);
+
+writeUserWelcome("Paul");
+*/
+
+/* userName = prompt("Please enter your username.", "");
+userAge = prompt("Please enter you age.", "");
+
+function writeUserWelcome (userName, userAge){
+	console.log("Welcome to my website " + userName + ".");
+	console.log("Hope you enjoy it.");
+	console.log("Your age is " + userAge + ".");
+}
+
+writeUserWelcome (userName, userAge);
+*/
+
+function outPtMsg(){
+	console.log("Hello World");
+
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+
+}
+
+calcArea();
+
+
+
