e7be69f796153b655f515fc9c9d5146482130be6
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-07-29 21:32:06 -0500

    returning values

--- Week 4 Activity Files/Screencast - Functions/js/script.js
+++ Week 4 Activity Files/Screencast - Functions/js/script.js
@@ -59,17 +59,73 @@ calcArea();
 
 //Arguments and Parameters
 
-funcName(argument1, argument2);
+/*funcName(argument1, argument2);
 
 function funcName(parameter1, parameter2){
 	//code the function runds
 }
+*/
+
+/*var total = calcArea(30, 20);
+
+function calcArea(w, h){
+	var area = w * h;
+	return area; //function spitting the info out
+}
+console.log(total);*/
 
 
+/*function dogYears(age){
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.")
+}
 
+var age1 = Number(prompt("Please enter the age of your dog in human years.",""));
 
+var age2 = Number(prompt("Please enter your other dogs age in human years."));
 
+dogYears(age1);
+dogYears(age2);
+*/
 
+/*
+var width = 5;
 
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+console.log(width);
+calcArea();
+*/
 
+/*function dogYears(age){
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.")
+}
 
+var age1 = 4;
+dogYears(age1);
+dogYears(5);
+dogYears(4);
+*/
+
+/* var total = calcArea(30, 20);
+
+function calcArea(w, h){
+	var area = w * h;
+	return area;  //function is spitting the info out.
+}
+
+console.log(total);
+*/
+
+var calcArea = function(width, height){
+	var area = width * height;
+	return area;
+}
+
+var a = calcArea(20, 30);
+console.log(a);
