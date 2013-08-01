945d3c123b3e91b55a5089b6b48e06fbf7d29d8a
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-08-01 18:16:58 -0500

    I created a prompt to ask the user to enter the length of the hose being used.

--- WPF Class - Assignments/Functions_Wacky/js/script.js
+++ WPF Class - Assignments/Functions_Wacky/js/script.js
@@ -9,4 +9,8 @@
 
 // First value needed is to calculate is the diameter of the hose.
 
-var diameter = Number(prompt("Please enter the diameter of the fire hose.", 1.5);
+var diameter = Number(prompt("Please enter the diameter of the fire hose.", 1.5));
+
+// Second value needed to calculate is the length of the hose being used. 
+
+var sectionLength = Number(prompt("Please enter the total length of the hose you are using.", 150));
