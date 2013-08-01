dd9f0ca73baa0799364e32fda39b2c2f200cc1ee
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-08-01 17:08:25 -0500

    Changed the prompt from a normal prompt to myPrompt, which is a function I created lower in the page.  Since it's a normal function, it can be placed anywhere.

--- WPF Class - Assignments/Functions_Personal/js/script.js
+++ WPF Class - Assignments/Functions_Personal/js/script.js
@@ -5,7 +5,7 @@
 
 //Calculating a homeowner's tax bill and then splitting it up into monthly payments. 
 
-var assessedValue = Number(prompt("Please enter the current assessed value for your home.", 150000));
+var assessedValue = myPrompt("Please enter the current assessed value for your home.", 150000, "Sorry, but you must enter a number for the value of your home.");  
 
 var millageRate = Number(prompt("Please enter the millage rate set by the tax office.", 5.0));
 
