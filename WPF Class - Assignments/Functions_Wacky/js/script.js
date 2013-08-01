975a273af6165b8be37dd52f012df06d56ea2307
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-08-01 18:15:09 -0500

    Created a variable to prompt the user to enter the diameter of the hose.

--- WPF Class - Assignments/Functions_Wacky/js/script.js
+++ WPF Class - Assignments/Functions_Wacky/js/script.js
@@ -4,3 +4,9 @@
 // Functions_Wacky
 
 // Fire Hose Friction Loss Formula
+
+// The formula for calculating friction loss is  FL = C * (Q/100)^2 * L/100
+
+// First value needed is to calculate is the diameter of the hose.
+
+var diameter = Number(prompt("Please enter the diameter of the fire hose.", 1.5);
