a6f863b5b60032cc8218f4f1170677ddf08e557c
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-08-02 14:07:26 -0500

    Added a variable that prompts the user to enter how many years the hits will be tracked.

--- WPF Class - Assignments/Functions_Industry/js/script.js
+++ WPF Class - Assignments/Functions_Industry/js/script.js
@@ -13,12 +13,20 @@
 //prompting the user to enter number of hits per day
 var hitsPerDay = prompt("Please enter the number of hits your website gets per day.", 3000);
 
-if(hitsPerDay != "" && !isNaN(hitsPerDay)){
-	console.log("The number of hits you entered is " + hitsPerDay + ".");
-}else{
+	if(hitsPerDay != "" && !isNaN(hitsPerDay)){
+		console.log("The number of hits you entered is " + hitsPerDay + ".");
+	}else{
 	console.log("You have entered \"" + hitsPerDay + "\", which is an invalid value.  Please refreash the webpage and try again.  Thank you.");
 }
 
 
 //Prompting the user to enter the number of years he wants forcasted. 
 
+var howManyYears = prompt("Please enter years this will be tracked." , 3);
+
+//	if(howManyYears != "" && !isNaN(howManyYears)){
+	//	console.log("You have entered " + howManyYears + ".");
+//	}else{
+//		console.log("You have entered and invalid number \"" + howManyYears + "\", whichi is an invalid value.  Please refreash the webpage and try again. Thank you.");
+//	}
+	
