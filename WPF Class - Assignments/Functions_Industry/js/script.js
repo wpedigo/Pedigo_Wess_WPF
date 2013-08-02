149d3bc7db20382d6496556df788ba17adbd5cbe
Wess Pedigo <wesspedigo@unknown4c8d79ef29c4.gateway.2wire.net>
2013-08-02 13:02:15 -0500

    Commented on what is needing to be calculated in or to get the total of real hits a website gets within a specific time frame.

--- WPF Class - Assignments/Functions_Industry/js/script.js
+++ WPF Class - Assignments/Functions_Industry/js/script.js
@@ -2,3 +2,10 @@
 // August 1,2013
 // Assigment: Functions
 // Functions_Industry
+
+// How many real hits your website get within a certain time frame?
+
+// totalHits = (hits * 365 * year)
+// spamHits = totalHits * (percent / 100) 
+// realHits = totalHits - spamHits   
+
